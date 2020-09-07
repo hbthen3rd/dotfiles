33497f9d42a123cbcaf517fa9caa3dbe522187c9
hbthen3rd <hungb.nguyen@live.com>
2020-09-07 15:14:16 -0700

    grammar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -5,8 +5,8 @@ killall -q polybar
 # If all your bars have ipc enabled, you can also use 
 # polybar-msg cmd quit
 
-# Launch bar1 and bar2
+# Launch bar
 echo "---" | tee -a /tmp/polybar.log
 polybar statusbar >>/tmp/polybar.log 2>&1 & disown
 
-echo "Bars launched..."
+echo "Bar launched..."
